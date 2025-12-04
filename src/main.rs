2fb13cd80c147e20881a5aa9cc03d79675f868c7
haminic <hamkittichet@gmail.com>
2025-12-04 23:29:35 +0700

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,13 @@ mod camera;
 mod objects;
 mod prelude;
 
+use std::fs;
+
 use crate::camera::Camera;
 use crate::objects::hittable::{HitRecord, Hittable};
 use crate::objects::hittable_list::HittableList;
 use crate::objects::sphere::Sphere;
 use crate::prelude::*;
-
-fn ray_color(ray: &Ray, world: &dyn Hittable) -> Color {
-    let mut rec = HitRecord::new();
-    if world.hit(ray, Interval::from(0.0, INFINITY), &mut rec) {
-        return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
-    }
-
-    let unit_direction = ray.direction.unit_vector();
-    let a = 0.5 * (unit_direction.y + 1.0);
-    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
-}
 
 fn main() -> std::io::Result<()> {
     // Image setup
@@ -48,7 +39,7 @@ fn main() -> std::io::Result<()> {
     let pixel00_loc = viewport_upper_left + 0.5 * pixel_delta_u + 0.5 * pixel_delta_v;
 
     // Render
-    let file = File::create("out.ppm")?;
+    let file = get_output_file()?;
     let mut writer = BufWriter::new(file);
 
     writeln!(writer, "P3")?;
@@ -80,11 +71,22 @@ fn new_main() -> std::io::Result<()> {
     world.add(Rc::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     let cam = Camera::new(16.0 / 9.0, 400);
-    let file = File::create("out.ppm")?;
+    let file = get_output_file()?;
     let mut writer = BufWriter::new(file);
     cam.render(&mut writer, &world);
 
     Ok(())
+}
+
+fn ray_color(ray: &Ray, world: &dyn Hittable) -> Color {
+    let mut rec = HitRecord::new();
+    if world.hit(ray, Interval::from(0.0, INFINITY), &mut rec) {
+        return 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0));
+    }
+
+    let unit_direction = ray.direction.unit_vector();
+    let a = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
 }
 
 fn show_progress(progress: f64) {
@@ -100,4 +102,13 @@ fn show_progress(progress: f64) {
 
     print!("\r{}", bar);
     stdout().flush().unwrap();
+}
+
+fn get_output_file() -> std::io::Result<File> {
+    let mut path = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).to_path_buf();
+    path.push("output");
+    fs::create_dir_all(&path)?;
+    path.push("out.ppm");
+
+    File::create(path)
 }
