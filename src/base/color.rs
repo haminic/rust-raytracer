cb1c1ceaf5cabcb4ae93b90adc1f4f76efc08a01
haminic <hamkittichet@gmail.com>
2025-12-05 16:27:29 +0700

    Apply gamma correction

--- src/base/color.rs
+++ src/base/color.rs
@@ -5,10 +5,22 @@ use crate::prelude::*;
 
 pub type Color = Vec3;
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        linear_component.sqrt()
+    } else {
+        0.0
+    }
+}
+
 pub fn write_color(writer: &mut BufWriter<File>, pixel_color: Color) -> std::io::Result<()> {
-    let r = pixel_color.x;
-    let g = pixel_color.y;
-    let b = pixel_color.z;
+    let mut r = pixel_color.x;
+    let mut g = pixel_color.y;
+    let mut b = pixel_color.z;
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     let intensity = Interval::new(0.000, 0.999);
     let r_byte = (256.0 * intensity.clamp(r)) as i32;
