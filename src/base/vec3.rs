116fdab151e23fd9ec7090fc0cbc231468601a7a
haminic <hamkittichet@gmail.com>
2025-12-05 20:07:17 +0700

    Refactor Vec3

--- src/base/vec3.rs
+++ src/base/vec3.rs
@@ -15,19 +15,19 @@ pub type Point3 = Vec3;
 
 impl Vec3 {
     pub const fn new(x: f64, y: f64, z: f64) -> Self {
-        Vec3 { x, y, z }
+        Self { x, y, z }
     }
 
-    pub fn random(range: impl SampleRange<f64> + Clone) -> Vec3 {
-        Vec3::new(
+    pub fn random(range: impl SampleRange<f64> + Clone) -> Self {
+        Self::new(
             random_range(range.clone()),
             random_range(range.clone()),
             random_range(range.clone()),
         )
     }
 
-    pub fn random_unit_vector() -> Vec3 {
-        Vec3::new(
+    pub fn random_unit_vector() -> Self {
+        Self::new(
             random_normal_f64(),
             random_normal_f64(),
             random_normal_f64(),
@@ -35,8 +35,8 @@ impl Vec3 {
         .unit_vector()
     }
 
-    pub fn random_on_hemisphere(normal: Vec3) -> Vec3 {
-        let on_unit_sphere = Vec3::random_unit_vector();
+    pub fn random_on_hemisphere(normal: Self) -> Self {
+        let on_unit_sphere = Self::random_unit_vector();
         if on_unit_sphere.dot(normal) > 0.0 {
             on_unit_sphere
         } else {
@@ -52,20 +52,33 @@ impl Vec3 {
         self.length_squared().sqrt()
     }
 
-    pub fn dot(self, other: Vec3) -> f64 {
+    pub fn dot(self, other: Self) -> f64 {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
 
-    pub fn cross(self, other: Vec3) -> Vec3 {
-        Vec3::new(
+    pub fn cross(self, other: Self) -> Self {
+        Self::new(
             self.y * other.z - self.z * other.y,
             self.x * other.z - self.z * other.x,
             self.x * other.y - self.y * other.x,
         )
     }
 
-    pub fn unit_vector(self) -> Vec3 {
+    pub fn unit_vector(self) -> Self {
         self / self.length()
+    }
+
+    pub fn reflect(self, normal: Self) -> Self {
+        // TODO
+        unimplemented!()
+    }
+
+    // ior = index of refraction, ior_ratio = n1 / n2
+    pub fn refract(self, normal: Self, ior_ratio: f64) -> Self {
+        let cos_theta = self.dot(-normal).min(1.0);
+        let r_out_perp = ior_ratio * (self + cos_theta * normal);
+        let r_out_parallel = -(1.0 - r_out_perp.length_squared()).abs().sqrt() * normal;
+        r_out_perp + r_out_parallel
     }
 }
 
