f286b892ea14bc421411e4ab6faa405fe79316e1
haminic <hamkittichet@gmail.com>
2025-12-07 22:32:29 +0700

    Separate moving translations from stationary ones

---
+++ src/objects/translating.rs
@@ -0,0 +1,36 @@
+use crate::objects::{Aabb, Hit, Hittable};
+use crate::prelude::*;
+
+pub struct Translating {
+    object: Box<dyn Hittable>,
+    offset: Lerp<Vec3>,
+    bbox: Aabb,
+}
+
+impl Translating {
+    // Moves object from from offset1 to offset2 within shutter time
+    pub fn new(object: impl Hittable + 'static, offset1: Vec3, offset2: Vec3) -> Self {
+        let bbox1 = object.bounding_box() + offset1;
+        let bbox2 = object.bounding_box() + offset2;
+        Self {
+            object: Box::new(object),
+            offset: Lerp::new(offset1, offset2),
+            bbox: Aabb::enclosing(bbox1, bbox2),
+        }
+    }
+}
+
+impl Hittable for Translating {
+    fn hit(&self, ray: &Ray, t_range: Interval) -> Option<Hit> {
+        let offset = self.offset.at(ray.time);
+        let translated_ray = Ray::with_time(ray.origin - offset, ray.direction, ray.time);
+        self.object.hit(&translated_ray, t_range).map(|mut hit| {
+            hit.point += offset;
+            hit
+        })
+    }
+
+    fn bounding_box(&self) -> Aabb {
+        self.bbox
+    }
+}
