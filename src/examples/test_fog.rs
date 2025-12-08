7b62889c1d5ae9b3bce36e34d9b15dce80dac75e
RimuWachi <wachii.int@gmail.com>
2025-12-08 14:44:03 +0700

    change exmples in test_fog

--- src/examples/test_fog.rs
+++ src/examples/test_fog.rs
@@ -9,12 +9,12 @@ pub fn test_fog() -> (World, Camera){
     let material1: Arc<dyn Material> = Arc::new(Lambertian::new(Color::new(0.2, 0.60, 0.2)));
     world.add(Sphere::new(Point3::new(0.0,2.0,0.0), 2.0, material1));
 
-    let difflight: Arc<dyn Material> = Arc::new(DiffuseLight::new(Color::new(0.73, 0.73, 0.73) * 4.0 ));
+    let difflight: Arc<dyn Material> = Arc::new(DiffuseLight::new(Color::new(0.73, 0.73, 0.73) * 2.0 ));
     world.add(Sphere::new(Point3::new(3.0,1.0,-2.0), 1.5, difflight));
 
     let border: Arc<dyn Material> = Arc::new(Dielectric::new(1.5));
     let boundary: Sphere = Sphere::new(Point3::new(3.0, 1.5, 0.8), 1.8, border);
-    world.add(ConstantMedium::new(boundary, 0.8, Color::new(0.2, 0.4, 0.8)));
+    world.add(ConstantMedium::new(boundary, 1.2, Color::new(0.2, 0.4, 0.8)));
 
     let resolution = Resolution::with_aspect_ratio(16.0 / 9.0, 1200);
     let cam = Camera::new(
