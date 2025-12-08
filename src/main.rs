69c61fe067b0e6911e87c446b3c2c161e9aeaa5f
RimuWachi <wachii.int@gmail.com>
2025-12-08 14:14:37 +0700

    feat: add scene argument in main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,33 @@ mod render;
 
 use std::fs;
 use std::time::Instant;
+use std::io::{ErrorKind::InvalidInput, Error};
 
 use crate::examples::*;
 use crate::prelude::*;
 use crate::render::*;
 
-static SAMPLES_PER_PIXEL: i32 = 100; // For cornell_box should be 200
-static MAX_DEPTH: i32 = 50; // For cornell_box should be 50
+static SAMPLES_PER_PIXEL: i32 = 20; // For cornell_box should be 200
+static MAX_DEPTH: i32 = 10; // For cornell_box should be 50
+static SCENE_SELECTED: i32 = 5;
 
 fn main() -> std::io::Result<()> {
     let renderer = Renderer::new(SAMPLES_PER_PIXEL, MAX_DEPTH);
     let file = get_output_file()?;
 
-    // let (world, camera) = bouncing_balls(10);
-    // let (world, camera) = simple_light();
-    // let (world, camera) = cornell_box();
-    // let (world, camera) = test_fog();
-    let (world, camera) = cornell_smoke();
+    let (world, camera) = match SCENE_SELECTED {
+        1 => bouncing_balls(10),
+        2 => simple_light(),
+        3 => test_fog(),
+        4 => cornell_box(),
+        5 => cornell_smoke(),
+        _ => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "The selected scene does not exist",
+            ));
+        }
+    };
 
     let start_time = Instant::now();
     renderer.render(&camera, &world, file)?;
